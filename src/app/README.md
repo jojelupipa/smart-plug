8bec6c24a997aa0077eea47923dd8254c8529c98
jojelupipa <jesus_38b@hotmail.com>
2020-06-23 13:23:52 +0200

    Añadidas instrucciones de lanzamiento de la app de escritorio

---
+++ src/app/README.md
@@ -0,0 +1,3 @@
+Para lanzar la aplicación simplemente ejecute:
+
+```python3 app.py```
---
+++ src/app/app.py
@@ -0,0 +1,11 @@
+# This Python file uses the following encoding: utf-8
+
+# ------------------------------------------
+# --- Author: Jesús Sánchez de Lechina Tejada
+# ------------------------------------------
+
+import os
+
+if __name__ == "__main__":
+    os.chdir("src")
+    os.system("python3 mainwindow.py")
